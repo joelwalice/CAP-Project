4c2cb44d42a35deec1fb6fba7b369ea160029a09
JoelWalice <joelwalice@gmail.com>
2023-10-03 17:35:28 +0530

    Update followup.js

--- client/src/pages/home/counselee/followup.js
+++ client/src/pages/home/counselee/followup.js
@@ -62,7 +62,7 @@ const Counselee = () => {
       <div className="bg-gray-500 h-screen">
         <div className="flex items-center justify-between bg-gray-900 h-[80px] w-screen">
         <h1 className="text-4xl text-white cursor-pointer md:text-4xl lg:text-5xl font-semibold p-4">Counselee View</h1>
-          <div className="flex items-center gap-4">
+          <div className="hidden md:flex items-center gap-4">
             <select className='p-2 rounded-md' onChange={handleRoleChange}>
             {
               roles.map((role, index) => (
