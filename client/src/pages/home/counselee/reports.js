a4fea6a6153b4f786a090a2551e47acc12241243
JoelWalice <joelwalice@gmail.com>
2023-10-03 17:52:17 +0530

    Update reports.js

--- client/src/pages/home/counselee/reports.js
+++ client/src/pages/home/counselee/reports.js
@@ -60,7 +60,7 @@ const Counselle = () => {
   return (
     <div>
       <title>Counselee Page</title>
-      <div className="bg-gray-500 h-screen">
+      <div className="bg-gray-500 w-screen h-screen">
         <div className="flex items-center justify-between bg-gray-900 h-[80px] w-screen">
         <h1 className="text-3xl text-white cursor-pointer md:text-4xl lg:text-5xl font-semibold p-4">Counselee View</h1>
           <div className="hidden md:flex items-center gap-4">
